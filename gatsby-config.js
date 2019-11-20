f53c3c5540effc97655451bd57a456a0dbf8fff0
kolbyjazz <57075021+kolbyjazz@users.noreply.github.com>
2019-11-19 20:42:53 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,3 +33,26 @@ module.exports = {
     },
   ],
 }
+const queries = require("./src/utils/algolia")
+
+require("dotenv").config()
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby+Algolia`,
+    description: `How to setup Algolia search in Gatsby`,
+    author: `<your name>`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries,
+        chunkSize: 10000, // default: 1000
+      },
+    },
+    `gatsby-plugin-styled-components`,
+  ],
+}
